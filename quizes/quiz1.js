764d38ddf8c25f6371f7085f40770eddb1f00af6
aboelela924 <aboelela924x10@gmail.com>
2020-10-24 12:17:43 +0200

    Boilerplate Done

---
+++ quizes/quiz1.js
@@ -0,0 +1,26 @@
+const musicData = [
+    { artist: 'Adele', name: '25', sales: 1731000 },
+    { artist: 'Drake', name: 'Views', sales: 1608000 },
+    { artist: 'Beyonce', name: 'Lemonade', sales: 1554000 },
+    { artist: 'Chris Stapleton', name: 'Traveller', sales: 1085000 },
+    { artist: 'Pentatonix', name: 'A Pentatonix Christmas', sales: 904000 },
+    { artist: 'Original Broadway Cast Recording', 
+      name: 'Hamilton: An American Musical', sales: 820000 },
+    { artist: 'Twenty One Pilots', name: 'Blurryface', sales: 738000 },
+    { artist: 'Prince', name: 'The Very Best of Prince', sales: 668000 },
+    { artist: 'Rihanna', name: 'Anti', sales: 603000 },
+    { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
+];
+
+const modified = musicData.map(arrObject => `${arrObject.name} by ${arrObject.artist} sold ${arrObject.sales} copies`)
+console.log(modified)
+
+console.log("\n***-------------------------------------------------------***\n")
+
+const albumsWith10CharsOrMore = musicData.filter(album => album.name.length >= 10 && album.name.length <= 25)
+console.log(albumsWith10CharsOrMore)
+
+console.log("\n***-------------------------------------------------------***\n")
+
+const popular = musicData.filter(album => album.sales >= 1000000).map(album => `${album.artist} is a great performer.`)
+console.log(popular)
