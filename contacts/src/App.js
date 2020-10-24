2e90b2bb58ccda28fcdaa15b0302dba174664606
aboelela924 <aboelela924x10@gmail.com>
2020-10-24 19:26:27 +0200

    UI Done with static Data

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ContactList from "./ContactList"
+
+
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
 
 class App extends Component{
+
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+  onDeleteClick = (id) => { 
+    this.setState((prevState) =>({
+      contacts: prevState.contacts.filter(contact => contact.id !== id)
+    }))
+  }
+
   render(){
     return (
       <div>
-        Hello World
+        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteClick}/>
       </div>
     )
   }
---
+++ contacts/src/ContactList.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import PropTypes from "prop-types"
+import escapeRegExp from "escape-string-regexp"
+import sortBy from "sort-by"
+
+class ContactList extends Component{
+    static propTypes = {
+        contacts: PropTypes.array.isRequired, 
+        onDelete: PropTypes.func.isRequired
+    }
+
+    state = {
+        query: ""
+    }
+
+    onQueryChange = (value) => {
+        this.setState({ 
+            query: value.trim()
+        })
+    }
+
+    resetQuery = () => {
+        this.setState({
+            query: ""
+        })
+    }
+
+    render(){
+
+        const {contacts, onDelete} = this.props
+        const { query } = this.state
+
+
+        let filteredContacts = []
+
+        if(query){
+            const matcher = new RegExp(escapeRegExp(query), "i")
+            filteredContacts = contacts.filter(contact => matcher.test(contact.name))
+        }else{
+            filteredContacts = contacts
+        }
+        
+        filteredContacts.sort(sortBy("name"))
+
+        return(
+            <div className="list-contacts">
+                <div className="list-contacts-top">
+                    <input type="text" className="search-contacts" value={query} onChange={(event) => this.onQueryChange(event.target.value)}/>
+                </div>
+                {filteredContacts.length != contacts.length && (
+                    <div className="showing-contacts">
+                        <span>Now Showing {filteredContacts.length} of {contacts.length}</span>
+                        <button onClick={this.resetQuery}>Show all</button>
+                    </div>
+                )}
+                <ol className="contact-list">
+                    {filteredContacts
+                    .map((contact, index) => (
+                        <li className="contact-list-item" key={contact.id}>
+                            <div className="contact-avatar" style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}/>
+                            <div className="contact-details">
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button className="contact-remove" onClick={() => onDelete(contact.id)}>Remove</button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
+}
+export default ContactList
